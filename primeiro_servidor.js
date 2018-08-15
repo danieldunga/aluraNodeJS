715cd3724247c51fccbc07c2951ff020742bd631
daniel.braga.ext <daniel.braga.ext@sodexo.com>
2018-08-15 17:07:04 -0300

    apresentação para equipe de teste

--- primeiro_servidor.js
+++ primeiro_servidor.js
@@ -7,7 +7,7 @@ const porta = process.env.NODE_PORT
 require('http').createServer(function (pedido, resposta) {
     console.log(pedido.url)
     var retorno = "Oi... "
-    for (let index = 0; index <= 1; index++) {
+    for (let index = 0; index <= 10; index++) {
         retorno += "," + index
     }
     resposta.end(retorno)
